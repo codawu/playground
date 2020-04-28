75a166e7d28159655777a0b327a2403c785f2a8b
dawu.wang <dawu.wang@ihandysoft.com>
2020-04-28 09:22:15 +0800

    change test handlers

--- cmd/iris/middleware/main.go
+++ cmd/iris/middleware/main.go
@@ -2,22 +2,76 @@ package main
 
 import (
 	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/context"
 )
 
-func before(ctx iris.Context) {
-	_, _ = ctx.Write([]byte("Before"))
-	ctx.Next()
+/**
+1. 第一个handler执行完后没有调用"Next",会连续打印两个"No",调用结束
+*/
+func NoNext(app *iris.Application) {
+	app.Get("/no/next", func(ctx context.Context) {
+		_, _ = ctx.Write([]byte("No"))
+		_, _ = ctx.Write([]byte("No"))
+	}, func(ctx context.Context) {
+		_, _ = ctx.Write([]byte("Next"))
+	})
 }
 
-func after(ctx iris.Context) {
-	ctx.Next()
-	_, _ = ctx.Write([]byte("After"))
+/**
+	1. 第一个handler执行完后调用了"Next"，会继续执行第二个函数
+	2. 第二个函数会连续打印两个"Next",然后回退到第一个函数
+    3. 第一个函数继续执行"Next"函数之后的代码，打印"No"
+*/
+func LastNoNext(app *iris.Application) {
+	app.Get("/no/next",
+		func(ctx context.Context) {
+			_, _ = ctx.Write([]byte("No"))
+			ctx.Next()
+			_, _ = ctx.Write([]byte("No"))
+		}, func(ctx context.Context) {
+			_, _ = ctx.Write([]byte("Next"))
+			_, _ = ctx.Write([]byte("Next"))
+		})
 }
 
-func HelloWorld(ctx iris.Context) {
-	_, _ = ctx.Write([]byte("HelloWorld"))
+/**
+	1. 第一个handler执行完后调用了"Next"，会继续执行第二个函数
+	2. 第二个函数会连续打印两个"Next",然后回退到第一个函数
+    3. 第一个函数继续执行"Next"函数之后的代码，打印"No"
+
+	第三个函数不会被执行
+*/
+func SecondLastNoNext(app *iris.Application) {
+	app.Get("/no/next",
+		func(ctx context.Context) {
+			_, _ = ctx.Write([]byte("No"))
+			ctx.Next()
+			_, _ = ctx.Write([]byte("No"))
+		}, func(ctx context.Context) {
+			_, _ = ctx.Write([]byte("Next"))
+			_, _ = ctx.Write([]byte("Next"))
+		},
+		func(ctx context.Context) {
+			_, _ = ctx.Write([]byte("Second"))
+			_, _ = ctx.Write([]byte("Second"))
+		})
 }
 
-func InitRouter(app *iris.Application) {
-	app.Get("/hello/world", before, after, HelloWorld)
+/**
+	1. 第一个handler执行完后调用了"Next"，会继续执行第二个函数
+	2. 第二个函数会打印一个"Next",然后执行"Next"，因为第二个函数之后没有继续执行的函数，故不产生任何操作
+		继续打印"Next"
+    3. 第一个函数继续执行"Next"函数之后的代码，打印"All"
+*/
+func AllNext(app *iris.Application) {
+	app.Get("/all/next",
+		func(ctx context.Context) {
+			_, _ = ctx.Write([]byte("All"))
+			ctx.Next()
+			_, _ = ctx.Write([]byte("All"))
+		}, func(ctx context.Context) {
+			_, _ = ctx.Write([]byte("Next"))
+			ctx.Next()
+			_, _ = ctx.Write([]byte("Next"))
+		})
 }
--- cmd/iris/middleware/main_test.go
+++ cmd/iris/middleware/main_test.go
@@ -1,19 +1,72 @@
 package main
 
 import (
+	"fmt"
 	"github.com/kataras/iris/v12"
 	"github.com/kataras/iris/v12/httptest"
 	"testing"
+)
+
+const (
+	NoNextStr           = "NoNo"
+	LastNoNextStr       = "NoNextNextNo"
+	SecondLastNoNextStr = "NoNextNextNo"
+	AllNextStr          = "AllNextNextAll"
 )
 
 /**
 1. Middleware is executed by peeling onions
 2. "Next" function must be added in middleware
 */
-func TestHelloWorld(t *testing.T) {
+func TestNoNext(t *testing.T) {
 	app := iris.New()
-	InitRouter(app)
+	NoNext(app)
 	ht := httptest.New(t, app)
-	want := "BeforeHelloWorldAfter"
-	ht.GET("/hello/world").Expect().Status(httptest.StatusOK).Body().Equal(want)
+	want := fmt.Sprintf("%s", NoNextStr)
+	ht.GET("/no/next").Expect().Status(httptest.StatusOK).Body().Equal(want)
+	if !t.Failed() {
+		t.Logf("✌️️️✌️️️✌️️️✌️️️✌️️️")
+	} else {
+		t.Logf("💔💔💔💔💔")
+	}
+}
+
+func TestLastNoNext(t *testing.T) {
+	app := iris.New()
+	LastNoNext(app)
+	ht := httptest.New(t, app)
+	want := fmt.Sprintf("%s", LastNoNextStr)
+	ht.GET("/no/next").Expect().Status(httptest.StatusOK).Body().Equal(want)
+	if !t.Failed() {
+		t.Logf("✌️️️✌️️️✌️️️✌️️️✌️️️")
+	} else {
+		t.Logf("💔💔💔💔💔")
+	}
+}
+
+func TestSecondLastNoNext(t *testing.T) {
+	app := iris.New()
+	SecondLastNoNext(app)
+	ht := httptest.New(t, app)
+	want := fmt.Sprintf("%s", SecondLastNoNextStr)
+	ht.GET("/no/next").Expect().Status(httptest.StatusOK).Body().Equal(want)
+	if !t.Failed() {
+		t.Logf("✌️️️✌️️️✌️️️✌️️️✌️️️")
+	} else {
+		t.Logf("💔💔💔💔💔")
+	}
+}
+
+func TestAllNext(t *testing.T) {
+	app := iris.New()
+	AllNext(app)
+	ht := httptest.New(t, app)
+	want := fmt.Sprintf("%s", AllNextStr)
+	ht.GET("/all/next").Expect().Status(httptest.StatusOK).Body().Equal(want)
+	if !t.Failed() {
+		t.Logf("✌️️️✌️️️✌️️️✌️️️✌️️️")
+	} else {
+		t.Logf("💔💔💔💔💔")
+	}
+
 }
